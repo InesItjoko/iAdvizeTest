cb05fa48636844cd7ef739d777e3f193edd31829
Thomas <Thomas>
2017-03-31 00:16:16 +0200

    pouet

--- home1.php
+++ home1.php
@@ -15,7 +15,8 @@ $rowCount=5;
 		<title>iAdvice exercice</title>
 		<meta http-equiv="Content-Type"  charset="iso-8859-1" />   
 		<link rel="stylesheet" type="text/css" href="css/home1.css" />
-    	<script src="tutorial01.slider/js/jssor.slider-23.0.0.min.js" type="text/javascript"></script>
+    	<!--<script src="tutorial01.slider/js/jssor.slider-23.0.0.min.js" type="text/javascript"></script>-->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     	<script src="utils/ga.js" type="text/javascript"></script>
 		<!--<script type="text/javascript" src="js/caroussel_accueil.js"/>
 		<link rel="stylesheet" type="text/css" href="css/caroussel_accueil.css" />-->
